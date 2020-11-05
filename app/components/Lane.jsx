398d25b9adfb7bc5ee21ce22ca5ae1efb5bc3325
Omar Jacobo García <omar_192222@live.com>
2020-11-05 15:48:25 -0600

    Divide LaneHeader from Lane
    Divide LaneHeader from Lane.

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import uuid from 'uuid';
 import connect from '../libs/connect';
 import LaneActions from '../actions/LaneActions';
 import NoteActions from '../actions/NoteActions';
 import Notes from './Notes';
+import LaneHeader from './LaneHeader';
 
 const Lane = ({
 	lane, notes, LaneActions, NoteActions, ...props
 }) => {
 	const editNote = (id, task) => {
 		NoteActions.update({ id, task, editing: false });
-	};
-
-	const addNote = e => {
-		e.stopPropagation();
-		const noteId = uuid.v4();
-		NoteActions.create({
-			id: noteId,
-			task: 'New Task'
-		});
-		LaneActions.attachToLane({
-			laneId: lane.id,
-			noteId
-		});
 	};
 
 	const deleteNote = (noteId, e) => {
@@ -40,12 +27,7 @@ const Lane = ({
 
 	return (
 		<div {...props}>
-			<div className="lane-header">
-				<div className="lane-add-note">
-					<button onClick={addNote}>+ Añadir Nota</button>
-				</div>
-				<div className="lane-name">{lane.name}</div>
-			</div>
+			<LaneHeader lane={lane} />
 			<Notes
 				notes={selectNotesByIds(notes, lane.notes)}
 				onNoteClick={activateNoteEdit}
