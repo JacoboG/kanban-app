fc1fd8b7fc0988592fb5166044a019c4e74c2f77
Omar Jacobo García <omar_192222@live.com>
2020-11-06 17:33:30 -0600

    Implementin onMove with state monitor
    Implementin onMove with state monitor and something of style.

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -5,6 +5,7 @@ import ItemTypes from '../constants/itemTypes';
 
 const Note = ({
 	connectDragSource, connectDropTarget,
+	isDragging, isOver,
 	onMove, id,
 	children, ...props
 }) => {
@@ -36,10 +37,12 @@ const noteTarget = {
 };
 
 export default compose(
-	DragSource(ItemTypes.NOTE, noteSource, connect => ({
-		connectDragSource: connect.dragSource()
+	DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging()
 	})),
-	DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
-		connectDropTarget: connect.dropTarget()
+	DropTarget(ItemTypes.NOTE, noteTarget, (connect, monitor) => ({
+		connectDropTarget: connect.dropTarget(),
+		isOver: monitor.isOver()
 	}))
 )(Note)
